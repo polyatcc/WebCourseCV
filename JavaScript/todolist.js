385b2ae089929a81ff7883d672cfc11f6848ef4b
Polya <ptceneva@gmail.com>
2021-12-15 04:40:50 +0300

    final lab5

--- JavaScript/todolist.js
+++ JavaScript/todolist.js
@@ -1,21 +1,22 @@
 //selecting dom elements for manipulation
 var input = document.querySelector("input[type = 'text']");
-var ul = document.querySelector("ul");
+var ul = document.querySelector("#todos");
+var zero = document.querySelector("#zeroEl");
 var container = document.querySelector("div");
 var lists = document.querySelectorAll("li");
 var spans = document.getElementsByTagName("span");
 var pencil = document.querySelector("#pencil");
-var saveBtn = document.querySelector(".save");
-var clearBtn = document.querySelector(".clear");
-var tipsBtn = document.querySelector(".tipBtn");
-var closeBtn = document.querySelector(".closeBtn");
+var saveBtn = document.querySelector("#save");
+var clearBtn = document.querySelector("#clear");
+var tipsBtn = document.querySelector("#tipBtn");
+var closeBtn = document.querySelector("#closebtn");
 var overlay = document.getElementById("overlay")
 
 
 //function to delete todo if delete span is clicked.
-function deleteTodo(){
-    for(let span of spans){
-        span.addEventListener ("click",function (){
+function deleteTodo() {
+    for (let span of spans) {
+        span.addEventListener("click", function() {
             span.parentElement.remove();
             event.stopPropagation();
         });
@@ -23,27 +24,28 @@ function deleteTodo(){
 }
 
 //function to load todo if list is found in local storage.
-function loadTodo(){
-    if(localStorage.getItem('todoList')){
+function loadTodo() {
+    if (localStorage.getItem('todoList')) {
         ul.innerHTML = localStorage.getItem('todoList');
         deleteTodo();
     }
 }
 
 //event listener for input to add new todo to the list.
-input.addEventListener("keypress",function(keyPressed){
-    if(keyPressed.which === 13){
+input.addEventListener("keypress", function(keyPressed) {
+    if (keyPressed.which === 13) {
+        // li > < span > < i class = "fas fa-trash-alt" > < /i></span > Go to the salon < /li>
         //creating lists and span when enter is clicked
         var li = document.createElement("li");
         var spanElement = document.createElement("span");
         var icon = document.createElement("i");
 
         var newTodo = this.value;
-        this.value = " " ;
+        this.value = " ";
 
         icon.classList.add('fas', 'fa-trash-alt');
         spanElement.append(icon);
-        ul.appendChild(li).append(spanElement,newTodo);
+        ul.appendChild(li).append(spanElement, newTodo);
 
         deleteTodo();
 
@@ -53,39 +55,38 @@ input.addEventListener("keypress",function(keyPressed){
 
 // event listener to linethrough list if clicked
 ul.addEventListener('click', function(ev) {
-        if (ev.target.tagName === 'LI') {
-            ev.target.classList.toggle('checked');
-        }
-    },false
-);
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
 
 //hide input box,when pencil icon is clicked
-pencil.addEventListener('click', function(){
+pencil.addEventListener('click', function() {
     input.classList.toggle('display');
 });
 
 
 
 //save todolist state so user can access it later
-saveBtn.addEventListener('click',function(){
-    localStorage.setItem('todoList',ul.innerHTML );
+saveBtn.addEventListener('click', function() {
+    localStorage.setItem('todoList', ul.innerHTML);
 
 });
 
 //clear all todo when clear button is clicked
-clearBtn.addEventListener('click', function(){
-    ul.innerHTML= "";
-    localStorage.removeItem('todoList',ul.innerHTML );
+clearBtn.addEventListener('click', function() {
+    ul.innerHTML = "";
+    localStorage.removeItem('todoList', ul.innerHTML);
 });
 
 //display overlay when tips btn is clicked
-tipsBtn.addEventListener("click",function(){
+tipsBtn.addEventListener("click", function() {
     overlay.style.height = "100%";
 });
 
 //close overlay when close btn is clicked
-closeBtn.addEventListener("click",function(e){
-    e.preventDefault;
+closeBtn.addEventListener("click", function(e) {
+    // e.preventDefault;
     overlay.style.height = "0";
 
 })
